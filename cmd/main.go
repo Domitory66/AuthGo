17ee783b9b13c3bef506dff257b9c6383d534e63
Domitory66 <borisyukd@inbox.ru>
2023-05-20 22:16:47 +0300

    set environment

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	"log"
+	"os"
 
 	authorization "github.com/Domitory66/AuthGo"
 	"github.com/Domitory66/AuthGo/pkg/handler"
 	"github.com/Domitory66/AuthGo/pkg/repository"
 	"github.com/Domitory66/AuthGo/pkg/service"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	err := initConfig()
-	if err != nil {
+	if err := initConfig(); err != nil {
 		log.Fatalf("error initialization configs %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASS"),
+		DBName:   viper.GetString("db.name"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(authorization.Server)
-	err = srv.Run(viper.GetString("port"), handlers.InitRoutes())
-	if err != nil {
+
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
